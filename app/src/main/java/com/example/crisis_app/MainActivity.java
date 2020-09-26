88b62ec58ae733740d67158dfd0eee59c6362fb8
Vladimir Hardy <vvhardy0143@eagle.fgcu.edu>
2020-09-26 15:39:06 -0400

    Linked create account button to its xml page

--- app/src/main/java/com/example/crisis_app/MainActivity.java
+++ app/src/main/java/com/example/crisis_app/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.crisis_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button createAccountBtn = (Button)findViewById(R.id.create_account_btn);
+        createAccountBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(v.getContext(), Signup.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+    }
+
+    public void launchSecondActivity(View v) {
+        Log.d(LOG_TAG, "Button pressed!");
+
         setContentView(R.layout.signup);
+
     }
 }
