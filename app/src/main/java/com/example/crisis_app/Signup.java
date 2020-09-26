cd39ef301223e4fca07686c6e71552611cddfa3c
Vladimir Hardy <vvhardy0143@eagle.fgcu.edu>
2020-09-26 19:07:11 -0400

    Fixed spinner text color

--- app/src/main/java/com/example/crisis_app/Signup.java
+++ app/src/main/java/com/example/crisis_app/Signup.java
@@ -1,8 +1,13 @@
 package com.example.crisis_app;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
@@ -19,11 +24,26 @@ public class Signup extends AppCompatActivity {
         spinner = (Spinner) findViewById(R.id.spinner);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.roles, android.R.layout.simple_spinner_item);
+                R.array.roles, R.layout.spinner_item);
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
 
+
+
     }
+
+    public AdapterView.OnItemSelectedListener OnCatSpinnerCL = new AdapterView.OnItemSelectedListener() {
+        public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+
+            ((TextView) parent.getChildAt(0)).setTextColor(Color.RED);
+            ((TextView) parent.getChildAt(0)).setTextSize(5);
+
+        }
+
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    };
 }
