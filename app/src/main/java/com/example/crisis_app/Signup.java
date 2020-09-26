abfbbdc3138306d55fc48536fd8789761ad2cc85
Vladimir Hardy <vvhardy0143@eagle.fgcu.edu>
2020-09-26 18:09:01 -0400

    Added spinner (change spinner text color to white)

--- app/src/main/java/com/example/crisis_app/Signup.java
+++ app/src/main/java/com/example/crisis_app/Signup.java
@@ -1,15 +1,29 @@
 package com.example.crisis_app;
 
-import android.app.AppComponentFactory;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class Signup extends AppCompatActivity {
+
+    Spinner spinner;
+
     /** Called when the activity is first created. */
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.roles, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
 
     }
 }
